727d7f5b56cf7543314316418e55f3cea5c7959d
Marcelo Carveche <marcelocarveche@gmail.com>
2025-05-01 23:13:30 -0400

    feat random color to links and logo

--- src/app/presentation/components/footer/header-footer.tsx
+++ src/app/presentation/components/footer/header-footer.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './header-footer-styles.module.scss'
 import { MailIcon, LinkedinIcon } from '@/app/presentation/components'
+import { colors } from '@/app/presentation/styles'
+import { useNavigate } from 'react-router-dom'
 
 interface Props {
   isFooter?: boolean
+}
+
+const ICON_COLORS = [
+  colors.orangeDefault,
+  colors.blueishDefault,
+  colors.pinkDark,
+  colors.greenDefault,
+  colors.yellowDark,
+]
+
+function getRandomColor() {
+  return ICON_COLORS[Math.floor(Math.random() * ICON_COLORS.length)]
 }
 
 const HeaderAndFooter: React.FC<any> = ({ children }) => {
@@ -17,16 +31,40 @@ const HeaderAndFooter: React.FC<any> = ({ children }) => {
 }
 
 const Component: React.FC<Props> = ({ isFooter = false }) => {
+  const navigate = useNavigate()
+  const [mailColor, setMailColor] = useState<string>(colors.black)
+  const [linkedinColor, setLinkedinColor] = useState<string>(colors.black)
+  const [logoColor, seLogoColor] = useState<string>(colors.black)
+
   return (
     <div data-footer={isFooter} className={styles.wrapper}>
       <div className={styles.content}>
-        <div className={styles.wrapLogo}>
+        <div
+          className={styles.wrapLogo}
+          onMouseEnter={() => seLogoColor(getRandomColor())}
+          onMouseLeave={() => seLogoColor(colors.black)}
+          onClick={() => navigate('/')}
+        >
           <span className={styles.firstName}>robin</span>
-          <span className={styles.lastName}>amorim</span>
+          <span style={{ color: logoColor }} className={styles.lastName}>
+            amorim
+          </span>
         </div>
         <div className={styles.wrapLinks}>
-          <MailIcon className={styles.mail} />
-          <LinkedinIcon className={styles.linkedin} />
+          <MailIcon
+            href="mailto:robin.amorim@outlook.com"
+            className={styles.mail}
+            color={mailColor}
+            onMouseEnter={() => setMailColor(getRandomColor())}
+            onMouseLeave={() => setMailColor(colors.black)}
+          />
+          <LinkedinIcon
+            href="https://www.linkedin.com/in/robinamorim/"
+            className={styles.linkedin}
+            color={linkedinColor}
+            onMouseEnter={() => setLinkedinColor(getRandomColor())}
+            onMouseLeave={() => setLinkedinColor(colors.black)}
+          />
         </div>
       </div>
     </div>
